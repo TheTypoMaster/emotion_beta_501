3cd169dd8b84d2d5d44f6fe9f7ae0aaba1dd6a57
placiano <placiano80@gmail.com>
2015-09-03 19:13:09 +0200

    POF2 Source Drop
    Signed-off-by: placiano <placiano80@gmail.com>

---
+++ ramdisk_emotion/fs/res/ext/sswap.sh
@@ -0,0 +1,28 @@
+#!/system/bin/sh
+
+
+PATH=/sbin:/system/sbin:/system/bin:/system/xbin
+export PATH
+
+
+# Inicio
+mount -o remount,rw -t auto /system
+mount -t rootfs -o remount,rw rootfs
+
+
+ZSWAPSIZE=1536
+
+
+
+
+if [ $ZSWAPSIZE -gt 0 ]; then
+echo `expr $ZSWAPSIZE \* 1024 \* 1024` > /sys/devices/virtual/block/vnswap0/disksize
+/system/xbin/busybox swapoff /dev/block/vnswap0 > /dev/null 2>&1
+/system/xbin/busybox mkswap /dev/block/vnswap0 > /dev/null 2>&1
+/system/xbin/busybox swapon /dev/block/vnswap0 > /dev/null 2>&1
+fi
+sync
+
+
+mount -t rootfs -o remount,ro rootfs
+mount -o remount,ro -t auto /system
